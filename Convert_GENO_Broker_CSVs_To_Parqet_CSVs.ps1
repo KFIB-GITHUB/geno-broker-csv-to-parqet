eee91bb86e9836ab9fc3ef367ad8dce6883a2685
kaiser friedrich i barbarossa <kfib@heidoetting.de>
2023-01-11 00:08:00 +0100

    initial commit

---
+++ Convert_GENO_Broker_CSVs_To_Parqet_CSVs.ps1
@@ -0,0 +1,94 @@
+﻿# Convert GENO Broker CSVs To Parqet CSVs
+# A PowerShell script to convert transaction CSV files from GENO Broker into CSVs files that can be imported as activities into Parqet.
+# License: Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+# Attribution: kfib@heidoetting.de
+
+function Select-Files {  
+
+  # Function to present the user with a files selector that enables the selection of multiple files.
+  # Snippet: https://stackoverflow.com/a/64481315
+  # License: Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+  # Attribution: https://stackoverflow.com/users/13496918/peter; https://stackoverflow.com/users/9898643/theo
+
+  [CmdletBinding()]  
+  Param (   
+    [Parameter(Mandatory = $false)]  
+    [string]$WindowTitle = 'Select File(s)',
+
+    [Parameter(Mandatory = $false)]
+    [string]$InitialDirectory,  
+
+    [Parameter(Mandatory = $false)]
+    [string]$Filter = '*All files (*.*)|*.*',
+
+    [switch]$AllowMultiSelect
+  ) 
+  Add-Type -AssemblyName System.Windows.Forms
+
+  $openFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+  $openFileDialog.Title = $WindowTitle
+  $openFileDialog.Filter = $Filter
+  $openFileDialog.CheckFileExists = $true
+  if (![string]::IsNullOrWhiteSpace($InitialDirectory)) { $openFileDialog.InitialDirectory = $InitialDirectory }
+  if ($AllowMultiSelect) { $openFileDialog.MultiSelect = $true }
+
+  if ($openFileDialog.ShowDialog().ToString() -eq 'OK') {
+    if ($AllowMultiSelect) { 
+      $selected = @($openFileDialog.Filenames)
+    } 
+    else { 
+      $selected = $openFileDialog.Filename
+    }
+  }
+  # clean-up
+  $openFileDialog.Dispose()
+
+  return $selected
+}
+
+# Present the user with a files selector that enables the selectin of multiple *.csv files, defaulting to 'My Documents'
+# Snippet: https://stackoverflow.com/a/64481315
+# License: Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+# Attribution: https://stackoverflow.com/users/13496918/peter; https://stackoverflow.com/users/9898643/theo
+
+$CSVfiles = Select-Files `
+  -WindowTitle 'Bitte zu konvertierende CSVs von GENO Broker auswählen / Please select GENO Broker CSVs for conversion' `
+  -Filter 'All files (*.csv)|*.csv' `
+  -InitialDirectory ([Environment]::GetFolderPath("MyDocuments")) `
+  -AllowMultiSelect
+
+$CSVfiles | Foreach-Object {
+
+  $FileContent = Get-Content -Path $_
+  $CSVContent = $FileContent | select-string -pattern @('^[^;\n]*((;[^;\n]*){10,}$)') -encoding ASCII
+  $CSVRecords = ConvertFrom-CSV $CSVContent -Delimiter ';'
+
+  $New = $CSVRecords | Select-Object `
+  @{N = 'broker'; E = { 'GENO Broker GmbH' } }, `
+  @{N = 'currency'; E = { $_.Währung } }, `
+  @{N = 'date'; E = { ([Datetime]::ParseExact($_.Datum, 'dd.MM.yyyy', $null)).tostring(“yyyy-MM-dd”) } }, `
+  @{N = 'fee'; E = { $_.Spesen } }, `
+  @{N = 'assetType'; E = { 'Security' } }, `
+  @{N = 'price'; E = { $_.'Ausf. kurs' -replace ('^0,0000$', '0,0001') } }, `
+  @{N = 'shares'; E = { [System.Convert]::ToDecimal($_.'Stück/Nominal', [cultureinfo]::GetCultureInfo('de-DE')) } }, `
+  @{N = 'tax'; E = { $_.Steuern } }, `
+  @{N = 'type'; E = { $_.Geschäftsart -replace ('^Kauf$', 'Buy') -replace ('^Verkauf$', 'Sell') -replace ('^Einlieferung$', 'TransferIn') -replace ('^Auslieferung$', 'TransferOut') -replace ('^Dividende$', 'Dividend') } }, `
+  @{N = 'wkn'; E = { $_.WKN } }, `
+  @{N = 'description'; E = { $_.Wertpapierbezeichnung } }
+
+  $counter = 0
+
+  $Path = $(Split-Path -Path $_)
+
+  $New | Sort-Object -Property date  | Foreach-Object {
+
+    $FileContentname = "$Path\$($_.date)_$($_.type)_$($_.wkn)_$(($_.description) -Replace('[^A-Za-z]',''))_$((++$counter)).csv"
+
+    $_ | Export-Csv -Path $FileContentname -Delimiter ";" -NoTypeInformation
+
+  }
+
+}
+
+
+
---
+++ README.md
@@ -0,0 +1,64 @@
+﻿# Convert GENO Broker CSVs To Parqet CSVs
+A PowerShell script to convert transaction CSV files from [GENO Broker](https://www.genobroker.de/) into CSVs files that can be imported as activities into [Parqet](https://www.parqet.com/).
+
+## Background
+
+[Parqet](https://www.parqet.com/) does not support importing transaction CSVs from [GENO Broker](https://www.genobroker.de/) but provides a [CSV template](https://www.parqet.com/blog/csv) to enable users to transform their transactions into a supported format.
+
+The objective of this PowerShell script is to convert transaction CSVs from [GENO Broker](https://www.genobroker.de/) into CSVs files that can be imported as activities into [Parqet](https://www.parqet.com/).
+
+## Attention
+
+The transaction CSV from [GENO Broker](https://www.genobroker.de/) has some deficiencies you should know before converting and importing them into Parqet.
+
+* The transactions in the [GENO Broker](https://www.genobroker.de/) CSV only have dates but not time
+* The transactions in the [GENO Broker](https://www.genobroker.de/) have, in rare cases, incorrect tax values (e.g., Transfer In of non € shares after Spin-off).
+
+## Prerequisites
+Before you begin, ensure you have met the following requirements:
+
+### [GENO Broker](https://www.genobroker.de/)
+* You have a brokerage account with [GENO Broker](https://www.genobroker.de/)
+* You have downloaded a transaction CSV file from [GENO Broker](https://www.genobroker.de/)
+  * Brokerage Classic => Depot => Depotumsätze => Liste als CSV exportieren
+
+### [Parqet](https://www.parqet.com/)
+* You have registered for a Basis/Plus/Investor account with [Parqet](https://www.parqet.com/)
+* You have created a portfolio at [Parqet](https://www.parqet.com/) ready for importing transactions
+
+### Microsoft Windows / PowerShell
+* You have a Windows 10 machine.
+* You have Windows PowerShell 5.1 installed.
+
+## Installation
+* Transfer the 'Convert_GENO_Broker_CSVs_To_Parqet_CSVs.ps1' to your machine.
+
+## Usage
+* Run 'Convert_GENO_Broker_CSVs_To_Parqet_CSVs.ps1' by right-clicking and selecting 'Run with Powershell'
+
+## Contributing
+To contribute, follow these steps:
+
+1. Fork this repository.
+2. Create a branch: `git checkout -b <branch_name>`.
+3. Make your changes and commit them: `git commit -m '<commit_message>'`
+4. Push to the original branch: `git push origin <project_name>/<location>`
+5. Create the pull request.
+
+Alternatively, see the GitHub documentation on [creating a pull request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request).
+
+## Attribution
+
+Thanks to the following people whose work I have been able to use in this project:
+
+[Selecting multiple documents in folder via powershell](https://stackoverflow.com/a/64481315)
+* License: Attribution-ShareAlike 4.0 International ([CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/))
+* Attribution: [peter](https://stackoverflow.com/users/13496918/peter); [theo](https://stackoverflow.com/users/9898643/theo)
+
+## Contact
+
+If you want to contact me, you can reach me at kfib@heidoetting.de.
+
+## License
+
+This project uses the following license: Attribution-ShareAlike 4.0 International ([CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/))
